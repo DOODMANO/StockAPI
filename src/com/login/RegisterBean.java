6f91a68bae46c194da169be58bbdddd79d13fb96
Bill Fang <williambillfang@gmail.com>
2017-12-16 15:34:51 -0500

    initialize term proj

---
+++ src/com/login/RegisterBean.java
@@ -0,0 +1,111 @@
+package com.login;
+
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class RegisterBean {
+
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String phonenumber;
+    private String email;
+    private String role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+    public String validateUser() {
+        boolean valid = RegisterDAO.validate(username, password, role);
+        if (valid == true) {
+        	if(this.role.equals("user"))
+            {
+        		return "userhome?faces-redirect=true";
+            }
+        	if(this.role.equals("manager")) //send request to admin
+            {
+        		return "managerhome?faces-redirect=true";
+            }
+        	else
+        	{
+        		System.out.println(this.role);
+        		System.out.println("ultimate failure");
+        		return "index";
+        	}
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_WARN, "Something went wrong.",""));
+            return "index";
+        }
+    }
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+}
---
+++ src/com/login/UserBean.java
@@ -0,0 +1,76 @@
+package com.login;
+
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class UserBean {
+
+    private String username;
+    private String password;
+    private String role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+    public String validateUser() {
+        boolean valid = LoginDAO.validate(username, password, role);
+        if (valid == true) {
+        	if(this.role.equals("user"))
+            {
+        		return "userhome?faces-redirect=true";
+            }
+        	if(this.role.equals("manager"))
+            {
+        		return "managerhome?faces-redirect=true";
+            }
+        	if(this.role.equals("admin"))
+            {
+        		return "adminhome?faces-redirect=true";
+            }
+        	else
+        	{
+        		System.out.println(this.role);
+        		System.out.println("ultimate failure");
+        		return "index";
+        	}
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_WARN, "Incorrect Username, Password or Role. Please enter correct them.",""));
+            return "index";
+        }
+    }
+
+    // logout event, invalidate session
+    public void logout() throws IOException {
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+    }
+
+
+}
